712d363f155d7c19a753fd890710863b0ebdb2bd
xxdd239 <60836285+xxdd239@users.noreply.github.com>
2024-09-19 20:20:32 +0800

    Update footer.tsx

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { $talc } from '../styles/utils'
 import styled from '@emotion/styled'
 import { Var, ThemeVar } from '../styles/variable'
@@ -10,9 +10,32 @@ const FooterContainer = styled.div`
 `
 
 export function Footer() {
+  const [ip, setIp] = useState('');
+
+  useEffect(() => {
+    const getLocalIP = () => {
+      const peerConnection = new RTCPeerConnection();
+      peerConnection.createDataChannel('');
+      peerConnection.createOffer().then(offer => peerConnection.setLocalDescription(offer));
+
+      peerConnection.onicecandidate = (event) => {
+        if (event && event.candidate && event.candidate.candidate) {
+          const candidate = event.candidate.candidate;
+          const ipMatch = candidate.match(/([0-9]{1,3}\.){3}[0-9]{1,3}/);
+          if (ipMatch) {
+            setIp(ipMatch[0]);
+          }
+        }
+      };
+    };
+
+    getLocalIP();
+  }, []);
+
   return (
     <FooterContainer>
-                         XPON-TEST 2024916
+      <div>XPON-TEST 2024916</div>
+      <div>客户端IP: {ip ? ip : 'Fetching IP...'}</div>
     </FooterContainer>
-  )
+  );
 }
